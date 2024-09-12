cbf9d98300a0f54ca8d51491d8c90490d35c41da
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2024-09-12 23:40:33 +0200

    Added intro, highlights & career info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,52 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Separator } from '$lib/components/ui/separator';
+	import githubLogo from '$lib/assets/github.svg';
+	import redditLogo from '$lib/assets/reddit.svg';
+	import discordLogo from '$lib/assets/discord.svg';
+
+	let { children } = $props();
 </script>
 
-<slot></slot>
+<svelte:head>
+	<title>Portfolio | EA-Wardie</title>
+</svelte:head>
+
+<ModeWatcher />
+
+<header class="h-14 grow-0 shrink-0 flex items-center justify-between border-b px-4">
+	<div class="h-full flex items-center gap-4">
+		<a href="/"><h3>EA</h3></a>
+		<Separator orientation="vertical" />
+	</div>
+	<nav class="h-full flex items-center gap-4">
+		<Separator orientation="vertical" />
+		<a href="https://github.com/EA-Wardie" target="_blank">
+			<img class="h-5 aspect-auto" alt="github" src={githubLogo}>
+		</a>
+		<a href="https://www.reddit.com/user/eawardie" target="_blank">
+			<img class="h-5 aspect-auto" alt="reddit" src={redditLogo}>
+		</a>
+		<a href="https://discordapp.com/users/214383922052595712" target="_blank">
+			<img class="h-5 aspect-auto" alt="discord" src={discordLogo}>
+		</a>
+	</nav>
+</header>
+<main class="w-full max-w-7xl mx-auto grow flex flex-col gap-12 p-4 md:p-12">
+	{@render children()}
+</main>
+<footer class="h-14 grow-0 shrink-0 flex items-center justify-between border-t px-4">
+	<div class="h-full flex items-center gap-4">
+		<div class="flex gap-1.5">
+			<p>{new Date().getFullYear()}</p>
+			<Separator class="min-h-5" orientation="vertical" />
+			<p>Made with <a class="hover:underline" href="https://kit.svelte.dev/" target="_blank">SvelteKit</a></p>
+		</div>
+		<Separator orientation="vertical" />
+	</div>
+	<nav class="h-full flex items-center gap-4">
+		<Separator orientation="vertical" />
+		<a class="hover:underline" href="/contact">Contact Me</a>
+	</nav>
+</footer>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+	import { Textarea } from '$lib/components/ui/textarea';
+	import { enhance } from '$app/forms';
+</script>
+
+<section class="text-center">
+	<h1>Contact Me</h1>
+</section>
+
+<section class="w-full flex justify-center">
+	<form class="w-full max-w-xl" method="POST" action="https://formsubmit.co/b008f795cef5a18f3e1b90389f53101f" use:enhance>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Want to get in touch?</Card.Title>
+				<Card.Description>Feel fee to send me a message below!</Card.Description>
+			</Card.Header>
+			<Card.Content class="flex flex-col gap-6">
+				<Label class="flex flex-col gap-1">
+					Full Name
+					<Input required autofocus name="name" placeholder="Enter your name" />
+				</Label>
+				<Label class="flex flex-col gap-1">
+					Email Address
+					<Input required type="email" name="email" placeholder="Enter your email" />
+				</Label>
+				<Label class="flex flex-col gap-1">
+					Message
+					<Textarea required name="message" placeholder="Enter a message" rows={7} />
+				</Label>
+			</Card.Content>
+			<Card.Footer class="justify-end">
+				<Button type="submit">Submit</Button>
+			</Card.Footer>
+		</Card.Root>
+	</form>
+</section>
