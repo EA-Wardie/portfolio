aff534b442dc33236f30e07cd0d780177dde6099
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2025-05-14 17:51:12 +0200

    Minor updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,80 +12,98 @@
 	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 
 	let { children } = $props();
-
 </script>
 
 <ModeWatcher />
 
 <header
-	class="sticky top-0 h-10 grow-0 shrink-0 bg-[hsl(var(--card))]/80 dark:bg-neutral-900/80 backdrop-blur flex items-center justify-between border-y z-10">
-	<nav class="grow flex">
+	class="sticky top-0 z-10 flex h-10 shrink-0 grow-0 items-center justify-between border-y bg-[hsl(var(--card))]/80 backdrop-blur dark:bg-neutral-900/80"
+>
+	<nav class="flex grow">
 		<Button variant="ghost" href="{base}/">Me</Button>
 		<Separator orientation="vertical" />
 		<Button variant="ghost" href="{base}/skills">Skills</Button>
 		<Separator orientation="vertical" />
-		<!--		<Button variant="ghost" href="{base}/projects">Projects</Button>-->
-		<!--		<Separator orientation="vertical" />-->
 		<Button variant="ghost" href="{base}/contact">Contact</Button>
 		<Separator orientation="vertical" />
 	</nav>
-	<nav class="shrink h-full flex items-center">
+	<nav class="flex h-full shrink items-center">
 		<Separator orientation="vertical" />
 		<Button variant="ghost" onclick={toggleMode}>
-			<LightDark class="w-6 h-6" />
+			<LightDark class="h-6 w-6" />
 		</Button>
 		<Separator orientation="vertical" />
 		<div class="hidden sm:flex">
 			<Button variant="ghost" href="https://github.com/EA-Wardie/" target="_blank">
-				<Github class="w-6 h-6" />
+				<Github class="h-6 w-6" />
 			</Button>
 			<Separator orientation="vertical" />
 			<Button variant="ghost" href="https://www.reddit.com/user/eawardie/" target="_blank">
-				<Reddit class="w-6 h-6" />
+				<Reddit class="h-6 w-6" />
 			</Button>
 			<Separator orientation="vertical" />
-			<Button variant="ghost" href="https://discordapp.com/users/214383922052595712/" target="_blank">
-				<Discord class="w-6 h-6" />
+			<Button
+				variant="ghost"
+				href="https://discordapp.com/users/214383922052595712/"
+				target="_blank"
+			>
+				<Discord class="h-6 w-6" />
 			</Button>
 		</div>
 		<AlertDialog.Root>
-			<AlertDialog.Trigger class="block sm:hidden {buttonVariants({variant: 'ghost'})}">
-				<Social class="w-6 h-6" />
+			<AlertDialog.Trigger class="block sm:hidden {buttonVariants({ variant: 'ghost' })}">
+				<Social class="h-6 w-6" />
 			</AlertDialog.Trigger>
 			<AlertDialog.Content>
 				<AlertDialog.Header>
 					<AlertDialog.Title>Get in touch</AlertDialog.Title>
 					<AlertDialog.Description>
 						<div class="flex justify-center gap-4 py-4">
-							<Button variant="outline" href="https://github.com/EA-Wardie/" target="_blank">
-								<Github class="w-8 h-8" />
+							<Button
+								variant="outline"
+								href="https://github.com/EA-Wardie/"
+								target="_blank"
+							>
+								<Github class="h-8 w-8" />
 							</Button>
-							<Button variant="outline" href="https://www.reddit.com/user/eawardie/" target="_blank">
-								<Reddit class="w-8 h-8" />
+							<Button
+								variant="outline"
+								href="https://www.reddit.com/user/eawardie/"
+								target="_blank"
+							>
+								<Reddit class="h-8 w-8" />
 							</Button>
-							<Button variant="outline" href="https://discordapp.com/users/214383922052595712/"
-									target="_blank">
-								<Discord class="w-8 h-8" />
+							<Button
+								variant="outline"
+								href="https://discordapp.com/users/214383922052595712/"
+								target="_blank"
+							>
+								<Discord class="h-8 w-8" />
 							</Button>
 						</div>
 					</AlertDialog.Description>
 				</AlertDialog.Header>
 				<AlertDialog.Footer>
-					<AlertDialog.Cancel class={buttonVariants({variant: 'default'})}>Close</AlertDialog.Cancel>
+					<AlertDialog.Cancel class={buttonVariants({ variant: 'default' })}
+						>Close</AlertDialog.Cancel
+					>
 				</AlertDialog.Footer>
 			</AlertDialog.Content>
 		</AlertDialog.Root>
 	</nav>
 </header>
-<main class="w-full max-w-7xl mx-auto grow flex flex-col gap-12 p-4 md:p-12">
+<main class="mx-auto flex w-full max-w-7xl grow flex-col gap-12 p-4 md:p-12">
 	{@render children()}
 </main>
 <footer
-	class="sticky bottom-0 h-10 grow-0 shrink-0 bg-[hsl(var(--card))]/80 dark:bg-neutral-900/80 backdrop-blur flex items-center justify-between border-y z-10">
+	class="sticky bottom-0 z-10 flex h-10 shrink-0 grow-0 items-center justify-between border-y bg-[hsl(var(--card))]/80 backdrop-blur dark:bg-neutral-900/80"
+>
 	<p class="px-4">{new Date().getFullYear()}</p>
 	<Separator orientation="vertical" />
 	<div class="grow"></div>
 	<Separator orientation="vertical" />
-	<Button variant="ghost" href="https://kit.svelte.dev/" class="gap-1"><p>Made with</p>
-		<p class="text-orange-500">SvelteKit</p></Button>
+	<Button variant="ghost" href="https://kit.svelte.dev/" class="gap-1"
+		><p>Made with</p>
+		<p class="text-orange-500">SvelteKit</p></Button
+	>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 	import { Badge } from '$lib/components/ui/badge';
 	import { Separator } from '$lib/components/ui/separator';
 	import Loader from '$lib/icons/Loader.svelte';
-	import { base } from '$app/paths';
 </script>
 
 <svelte:head>
@@ -12,37 +11,38 @@
 </svelte:head>
 
 <section class="flex flex-col max-sm:items-center">
-	<h1 class="-translate-x-1"><span class="text-sky-500">E</span>dward van <span class="text-emerald-500">T</span>onder
+	<h1 class="-translate-x-1">
+		<span class="text-sky-500">E</span>dward van <span class="text-emerald-500">T</span>onder
 	</h1>
 	<p>Welcome to my developer profile!</p>
 </section>
 
-<section class="flex flex-col-reverse sm:flex-row max-sm:items-center gap-6">
-	<div class="grow px-0.5 sm:px-0 max-sm:text-center">
+<section class="flex flex-col-reverse gap-6 max-sm:items-center sm:flex-row">
+	<div class="grow px-0.5 max-sm:text-center sm:px-0">
 		<p>
-			I am a <span class="text-rose-400">passionate</span> front-end developer with a focus on building dynamic
-			and responsive user interfaces.
+			I am a <span class="text-rose-400">passionate</span> front-end developer with a focus on
+			building dynamic and responsive user interfaces.
 		</p>
-		<br>
+		<br />
 		<p>
-			With a strong command of modern frameworks,
-			I specialize in creating seamless web experiences that are both
-			<span class="text-emerald-400">functional</span> and <span class="text-sky-400">visually appealing</span>.
-			I am committed to writing clean, maintainable code and staying
-			updated with the latest trends in web development.
+			With a strong command of modern frameworks, I specialize in creating seamless web
+			experiences that are both
+			<span class="text-emerald-400">functional</span> and
+			<span class="text-sky-400">visually appealing</span>. I am committed to writing clean,
+			maintainable code and staying updated with the latest trends in web development.
 		</p>
 	</div>
 	<Card.Root class="w-fit p-4">
-		<Avatar.Root class="w-64 h-64 rounded-none" delayMs={500}>
-			<Avatar.Image src="{base}/profile_image.jpg" />
+		<Avatar.Root class="h-64 w-64 rounded-none" delayMs={500}>
+			<Avatar.Image src="/profile_image.jpg" />
 			<Avatar.Fallback class="rounded-none bg-transparent">
-				<Loader class="w-20 h-20" />
+				<Loader class="h-20 w-20" />
 			</Avatar.Fallback>
 		</Avatar.Root>
 	</Card.Root>
 </section>
 
-<section class="grid grid-cols-1 md:grid-cols-3 gap-4">
+<section class="grid grid-cols-1 gap-4 md:grid-cols-3">
 	<Card.Root>
 		<Card.Header>
 			<Card.Title>Education</Card.Title>
@@ -50,9 +50,8 @@
 		</Card.Header>
 		<Card.Content>
 			<blockquote class="prose">
-				I successfully completed my formal education, where I gained foundational knowledge and skills that have
-				shaped my personal and
-				professional development.
+				I successfully completed my formal education, where I gained foundational knowledge
+				and skills that have shaped my personal and professional development.
 			</blockquote>
 		</Card.Content>
 	</Card.Root>
@@ -63,9 +62,9 @@
 		</Card.Header>
 		<Card.Content>
 			<blockquote>
-				I completed my university education with a degree in Computer Science & Information Technology, where I
-				gained in-depth
-				knowledge of software development, algorithms, and data structures.
+				I completed my university education with a degree in Computer Science & Information
+				Technology, where I gained in-depth knowledge of software development, algorithms,
+				and data structures.
 			</blockquote>
 		</Card.Content>
 	</Card.Root>
@@ -76,10 +75,9 @@
 		</Card.Header>
 		<Card.Content>
 			<blockquote>
-				With 7 years of hands-on experience in web development, I’ve had the opportunity to work on a diverse
-				range
-				of projects,
-				building and optimizing websites and web applications for various industries.
+				With 7 years of hands-on experience in web development, I’ve had the opportunity to
+				work on a diverse range of projects, building and optimizing websites and web
+				applications for various industries.
 			</blockquote>
 		</Card.Content>
 	</Card.Root>
@@ -98,57 +96,63 @@
 					<Badge class="rounded-full">Ongoing</Badge>
 				</div>
 				<article>
-					As a freelancer, I’ve gained valuable experience by working independently with diverse clients and
-					projects. This has
-					sharpened my ability to manage time effectively, meet tight deadlines, and adapt to different
-					requirements.
+					As a freelancer, I’ve gained valuable experience by working independently with
+					diverse clients and projects. This has sharpened my ability to manage time
+					effectively, meet tight deadlines, and adapt to different requirements.
 				</article>
 			</div>
 			<Separator class="w-full lg:max-w-xs" />
 			<div>
 				<div class="flex items-center gap-2">
-					<a class="text-green-400 hover:underline" href="https://xtrapolate.io/" target="_blank"><h4>
-						Xtrapolate</h4></a>
+					<a
+						class="text-green-400 hover:underline"
+						href="https://xtrapolate.io/"
+						target="_blank"><h4>Xtrapolate</h4></a
+					>
 					<Badge class="rounded-full">2018 - 2019</Badge>
 				</div>
 				<article>
-					During my time at Xtrapolate, I gained valuable experience working primarily with Laravel and Vue on
-					a variety of web
-					development projects. This role allowed me to deepen my expertise in building efficient back-end
-					systems with Laravel
+					During my time at Xtrapolate, I gained valuable experience working primarily
+					with Laravel and Vue on a variety of web development projects. This role allowed
+					me to deepen my expertise in building efficient back-end systems with Laravel
 					while creating dynamic, responsive front-end applications using Vue.
 				</article>
 			</div>
 			<Separator class="w-full lg:max-w-xs" />
 			<div>
 				<div class="flex items-center gap-2">
-					<a class="text-cyan-400 hover:underline" href="https://www.fitt.ai/" target="_blank">
+					<a
+						class="text-cyan-400 hover:underline"
+						href="https://www.fitt.ai/"
+						target="_blank"
+					>
 						<h4>Fitt Talent</h4>
 					</a>
 					<Badge class="rounded-full">2019 - 2023</Badge>
 				</div>
 				<article>
-					At Fitt Talent, I specialized in building dynamic, real-time KPI software using Laravel and Vue to
-					manage employee
-					rating and performance metrics. My work focused on developing systems that could process and
-					display real-time data
+					At Fitt Talent, I specialized in building dynamic, real-time KPI software using
+					Laravel and Vue to manage employee rating and performance metrics. My work
+					focused on developing systems that could process and display real-time data
 					efficiently, ensuring accurate and responsive workplace performance evaluations.
 				</article>
 			</div>
 			<Separator class="w-full lg:max-w-xs" />
 			<div>
 				<div class="flex items-center gap-2">
-					<a class="text-pink-500 hover:underline" href="https://www.snappi-group.com/" target="_blank"><h4>
-						Snappi Group of
-						Companies</h4></a>
+					<a
+						class="text-pink-500 hover:underline"
+						href="https://www.snappi-group.com/"
+						target="_blank"><h4>Snappi Group of Companies</h4></a
+					>
 					<Badge class="rounded-full">2023 - Now</Badge>
 				</div>
 				<article>
-					At Snappi Group of Companies, I focus on developing and maintaining e-commerce platforms, working
-					primarily on web
-					development projects that enhance online shopping experiences. I built and maintain scalable,
-					user-friendly solutions,
-					that streamline transactions, optimize product displays, and improved customer engagement.
+					At Snappi Group of Companies, I focus on developing and maintaining e-commerce
+					platforms, working primarily on web development projects that enhance online
+					shopping experiences. I built and maintain scalable, user-friendly solutions,
+					that streamline transactions, optimize product displays, and improved customer
+					engagement.
 				</article>
 			</div>
 		</Card.Content>
